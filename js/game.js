01877db6953cd67eaa5a180dc47a522270eb75ee
Mohammad Hussain <111192910+sain9@users.noreply.github.com>
2023-05-01 00:13:47 +0530

    treasure height at line number 100

--- js/game.js
+++ js/game.js
@@ -97,11 +97,11 @@ let boardContainer = document.getElementById('boardContainer');
         if(pointX == xAxisTreasurePoint && pointY == yAxisTreasurePoint){
             console.log("Yay ! you found the treasure...");
             // changeBoxColor(xAxis, yAxis, "green");
-            board[xAxis][yAxis].innerHTML = "<img src='./images/Treasure.png' height='85' width='85'>";
+            board[xAxis][yAxis].innerHTML = "<img src='./images/Treasure.png' height='75' width='75'>";
         }
         else { 
           // changeBoxColor(xAxis, yAxis, "white");
-          board[xAxis][yAxis].innerHTML = "<img src='./images/Hole_in_Ground.png' height='55' width='75'>";
+          board[xAxis][yAxis].innerHTML = "<img src='./images/Hole_in_Ground.png' height='75' width='75'>";
         }
 
         //to make hint div visible
